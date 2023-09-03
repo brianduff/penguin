ec26fc68d227781a863bde21489486bc49f2c4ba
Brian Duff <brian@dufflin.com>
2023-09-03 11:04:37 -0700

    Fix some bugs in generate

--- src/generate.rs
+++ src/generate.rs
@@ -9,14 +9,24 @@ static SQUID_DIR: &str = "squid";
 static DENY_RULE: &str = "deny_http_access";
 static ALLOW_RULE: &str = "allow_http_access";
 
-fn id_string<T: Identifiable>(item: &T) -> String {
-  format!("{:0>4}", item.id().unwrap())
+impl Identifiable for u32 {
+    fn id(&self) -> Option<u32> {
+        Some(*self)
+    }
+
+    fn set_id(&mut self, _: u32) {
+        unimplemented!()
+    }
+}
+
+fn id_string<T: Identifiable>(type_name: &str, item: &T) -> String {
+  format!("{}_{:0>4}", type_name, item.id().unwrap())
 }
 
 pub fn generate_squid_config(clients: &IdentifiedList<Client>, domainlists: &IdentifiedList<DomainList>) -> Result<()> {
   let base_path = Path::new(SQUID_DIR);
   for client in clients.items.iter().filter(|c| !c.rules.is_empty()) {
-    let client_name = id_string(client);
+    let client_name = id_string("client", client);
     let mut b = create_writer(base_path, format!("{}.conf", client_name))?;
 
     // First, figure out if there are any domains that are temporarily allowed due to a lease rule.
@@ -30,7 +40,7 @@ pub fn generate_squid_config(clients: &IdentifiedList<Client>, domainlists: &Ide
     b.writeln(format!("acl {} src {}/255.255.255.255", client_name, client.ip))?;
     for domain in client.rules.iter().filter(|r| r.kind == DENY_RULE).flat_map(|r| r.domainlists.iter() ) {
       if !allowed_domains.contains(&domain) {
-        b.writeln(format!("http_access deny {} domains_{}", client_name, domain))?;
+        b.writeln(format!("http_access deny {} {}", client_name, id_string("domains", domain)))?;
       }
     }
   }
@@ -39,7 +49,7 @@ pub fn generate_squid_config(clients: &IdentifiedList<Client>, domainlists: &Ide
     let mut b = create_writer(base_path, "domains.conf")?;
 
     for domainlist in domainlists.items.iter() {
-      let domainlist_name = id_string(domainlist);
+      let domainlist_name = id_string("domains", domainlist);
       b.writeln(format!("acl {} dstdomain \"./{}.txt\"", domainlist_name, domainlist_name))?;
       let mut b = create_writer(base_path, format!("{}.txt", domainlist_name))?;
       b.writeln(domainlist.domains.join("\n"))?;
