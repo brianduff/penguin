7480a1771acb6a335f9e495974ca10f92afe5c65
Brian Duff <brian@dufflin.com>
2023-09-12 14:31:21 -0700

    Switch to swc

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,11 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <p className="read-the-docs">
-        I am a penguin
+      <p>
+        I am penguin
       </p>
     </>
   )
