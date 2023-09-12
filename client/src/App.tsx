cac34881eacd0172de66f6b75c62f771a5e310b1
Brian Duff <brian@dufflin.com>
2023-09-12 14:27:39 -0700

    Initial revision of client

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p className="read-the-docs">
+        I am a penguin
+      </p>
+    </>
+  )
+}
+
+export default App
