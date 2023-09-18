d6c7d53d6396f39157570284291ac37d8789c1b8
Brian Duff <brian@dufflin.com>
2023-09-18 14:30:32 -0700

    refactor(client): Add table component

---
+++ client/src/components/Table.tsx
@@ -0,0 +1,26 @@
+import { HTMLTable } from "@blueprintjs/core";
+import { ReactElement } from "react";
+
+export interface TableProps {
+  columnNames: string[],
+  children: ReactElement[] | undefined | boolean
+}
+
+export function Table({ columnNames, children }: TableProps) {
+  if (!children) {
+    return <></>;
+  }
+
+  return (
+    <HTMLTable compact={true} striped={true}>
+      <thead>
+        <tr>
+          {columnNames.map(c => <th>{c}</th>)}
+        </tr>
+      </thead>
+      <tbody>
+        {children}
+      </tbody>
+    </HTMLTable>
+  );
+}
