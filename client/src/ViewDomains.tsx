ea528aa99216e5ec86d8c6142290497a484bcd9e
Brian Duff <brian@dufflin.com>
2023-09-25 22:02:03 -0700

    feat(client): begin to add edit domain name

--- client/src/ViewDomains.tsx
+++ client/src/ViewDomains.tsx
@@ -8,6 +8,7 @@ import { useState } from "react";
 import { DNSInputField } from "./Domains";
 import { clone } from "./ViewClient";
 import { updateDomainList } from "./api";
+import { css } from "@emotion/react";
 
 export function ViewDomains() {
   const domains = useLoaderData() as Result<DomainList>;
@@ -39,6 +40,12 @@ export function ViewDomains() {
   return (
     <Section title="Entries" icon={<GlobeNetwork />}>
       <SectionCard>
+        <div css={css`display: grid; grid-template-columns: auto 1fr; grid-gap: 10px;`}>
+          <span>Name:</span>
+          {/* <FieldEditor onSubmit={commitClient} field="name" original={client} /> */}
+        </div>
+
+
         {domains.unwrap().domains &&
         <Table columnNames={["DNS name", ""]}>
           {domains.unwrap().domains.map((domain, index) => (
